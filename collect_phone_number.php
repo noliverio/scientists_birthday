6d6a7550766aa1bfa7f05bfbb0abc2f7fee2192e
noliverio <nick.oliverio@comcast.net>
2015-03-20 22:41:43 -0400

    added a confirmation page to registration process.

--- collect_phone_number.php
+++ collect_phone_number.php
@@ -1,6 +1,10 @@
 <?PHP
 // connect to database, '$Database_connection' acts as the link
 require('../../mysqli_connection.php');
+// allows me to debug the script
+ini_set('display errors', 1);
+error_reporting(E_ALL | E_STRICT);
+
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $errors = array();
     if(!empty($_POST['name'])){
@@ -8,50 +12,35 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     }else{
         $errors[] = 'You need to enter a name';
     }
+// To get through the js the phone number should be 10 digit long number, but this acts as a second check
     if(!empty($_POST['phone_number'])){
-        $phone_number = mysqli_real_escape_string($Database_connection, trim($_POST['phone_number']));
+        $regex_pattern = '/^\d{10}$/';
+        if(preg_match($regex_pattern, mysqli_real_escape_string($Database_connection, trim($_POST['phone_number'])))){
+            $phone_number = mysqli_real_escape_string($Database_connection, trim($_POST['phone_number']));
+        }else{
+            $errors[] = 'You need to enter your ten digit phone number using only numbers';
+            $phone_number = null;
+        }
     }else{
         $errors[] = 'you must enter a phone number';
     }
-    $provider_to_domain = array(
-           "ATT" => "txt.att.net",
-           "Bell_canada" => "txt.bell.ca",
-           "Boost_mobile" => "myboostmobile.com",
-           "Metro_pcs" => "mymetropcs.com",
-           "Sprint" => "messaging.sprintpcs.com",
-           "T_mobile" => "tmomail.net",
-           "Verizon" => "vtext.com",
-           "Virgin_mobile" => "vmobl.com",
-           );
+
     if(!empty($_POST['service_provider'])){
-        if(array_key_exists($_POST['service_provider'], $provider_to_domain)){
-            $provider = mysqli_real_escape_string($Database_connection, trim($_POST['service_provider']));
-        }else{
-            $provider = false;
-            $errors[] = 'Currently only the service providers listed are supported.';
-        }
+        $provider = mysqli_real_escape_string($Database_connection, trim($_POST['service_provider']));
     }else{
         $errors[] = 'you must enter a service provider';
-        $provider = false;
+        $provider = null;
     }
-    $active = 0;
-    $confirmation_number = rand(10000, 99999);
 
-// The phone number should be 10 digit long number, but this acts as a second check
-    $regex_pattern = "/^\d{10}$/";
-    If (preg_match($regex_pattern, $phone_number) && $provider){
-        $email_address = $phone_number.'@'.$provider_to_domain[$provider];
-    }else{
-        $errors[] = 'You need to enter your ten digit phone number using only numbers';
-        $email_address = null;
-    }
     if(!empty($errors)){
         foreach($errors as $error){
             echo "<p>$error</p>";
         }
         echo"<a href = './'>click here to go back and fix that</a>";
     }else{
-        $query = "INSERT INTO users(email, name, active, confirmation_number) VALUES ('$email_address', '$name', '$active', '$confirmation_number')";
+        $active = 0;
+        $confirmation_number = rand(10000, 99999);
+        $query = "INSERT INTO users(phone_number, name, active, confirmation_number, service_provider) VALUES ('$phone_number', '$name', '$active', '$confirmation_number', (SELECT provider_id FROM provider WHERE name = '$provider'));";
         $request = mysqli_query($Database_connection, $query);
         if($request){
             $body_message = "<p>You have successfully registered, look for a text with a confirmation number and enter it on the next page</p>";
@@ -60,8 +49,10 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
             $return_page = "<!Doctype html>\n<html>\n<head>\n<meta charset = utf8>\n</head>\n<body>\n$body_message\n$pass_phone_number\n$redirect_script\n</body>\n</html>";
             echo $return_page;
         }else{
-            echo"<p>Sorry there was a problem registering you, please try again later.</p>";
+            echo 'error';
         }
     }
 }
+echo 'here';
+mysqli_close($Database_connection);
 ?>
--- confirm_number.php
+++ confirm_number.php
@@ -7,8 +7,8 @@ error_reporting(E_ALL | E_STRICT);
 // if the user navigates to this page directly, nothing should happen.
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $errors = array();
-    if(!empty($_POST['Phone_number'])){
-        $phone_number = mysqli_real_escape_string($Database_connection, trim($_POST['Phone_number']));
+    if(!empty($_POST['phone_number'])){
+        $phone_number = mysqli_real_escape_string($Database_connection, trim($_POST['phone_number']));
     }else{
         $phone_number = null;
         $errors[] = "<p>You must enter you phone number</p>";
@@ -17,16 +17,32 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $confirmation_number = mysqli_real_escape_string($Database_connection, trim($_POST['confirmation_number']));
     }else{
         $confirmation_number = null;
-        $errors[] = "<p>You must enter the five digit confirmation number sent to your phone</p>",
+        $errors[] = "<p>You must enter the five digit confirmation number sent to your phone</p>";
     }
+    // if the form is valid, check if the confirmation number is right. If it is activate the user.
     if(empty($errors)){
-        $get_confirmation_number_query = "";
-        $confirmation_number_from_db = mysqli_query($Database_connection, $query);
-        if($confirmation_number
-        
+        $get_confirmation_number_query = "SELECT confirmation_number FROM users WHERE phone_number = '$phone_number' ";
+        $confirmation_number_mysql_format = mysqli_query($Database_connection, $get_confirmation_number_query);
+        while($confirmation_number_array = mysqli_fetch_array($confirmation_number_mysql_format, MYSQLI_ASSOC)){
+            $confirmation_number_from_db = $confirmation_number_array['confirmation_number'];
+        }
+        if($confirmation_number == $confirmation_number_from_db){
+            $confirm_user_query = "UPDATE users SET active = 1 WHERE phone_number = $phone_number";
+            $activate_user = mysqli_query($Database_connection, $confirm_user_query);
+            if($activate_user){
+                echo "<p>Thank you for confirming your registration, you will now begin receiving text messages.</p>";
+            }else{
+            echo"<h1>System Error</h1>";
+            echo"<p>You could not be confirmed due to a system error.</p>";
+            echo'<p>'.mysqli_error($Database_connection).'<br /><br />Query: '.$query.'</p>';
+            }
+        }else{
+            echo "Sorry it doesn't look like that is number that was sent to you ";
+        }
     }else{
         foreach($errors as $error){
             echo $error;
         }
+    }
 }
 ?>
